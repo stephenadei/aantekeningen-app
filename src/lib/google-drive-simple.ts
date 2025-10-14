78bca307c16e8f0719f58a40a511efb126b941f4
Stephen Adei <s.adei@outlook.com>
2025-10-14 18:43:13 +0200

    fix: Revert thumbnail URL to working Google Drive format
    - Change back from googleusercontent to drive.google.com/thumbnail
    - This should resolve the 400 errors for thumbnail loading
    - Keep the direct PDF preview URL for modal functionality

--- src/lib/google-drive-simple.ts
+++ src/lib/google-drive-simple.ts
@@ -320,7 +320,7 @@ class GoogleDriveService {
           url: `https://drive.google.com/file/d/${file.id}/view`,
           downloadUrl: `https://drive.google.com/uc?export=download&id=${file.id}`,
           viewUrl: `https://drive.google.com/file/d/${file.id}/preview`,
-          thumbnailUrl: `https://lh3.googleusercontent.com/d/${file.id}=w400-h300`,
+          thumbnailUrl: `https://drive.google.com/thumbnail?id=${file.id}&sz=w400-h300`,
           modifiedTime: file.modifiedTime,
           size: parseInt(file.size || '0'),
           // AI-generated metadata
